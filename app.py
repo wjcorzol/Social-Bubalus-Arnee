8174a59359322c6c998cd1373ff7696553920d6f
FabianAbella <fabian.ing95@gmail.com>
2021-10-16 13:55:42 -0500

    Sincronización de páginas de HTML y CSS's por medio de Flask - Funcional

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask import render_template as render
+
+app= Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def inicio():
+    return render("home.html")
+
+@app.route("/registro", methods=["GET", "POST"])
+def registro():
+    return render("registro.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render("login.html")
+
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard():
+    return render("dashboard.htmal")
+
+@app.route("/perfilusuario", methods=["GET", "POST"])
+def perfilusuario():
+    return render("perfil.html")
+
+@app.route("/configuracion", methods=["GET", "POST"])
+def configuracion():
+    return render("configuracion.html")
+
+@app.route("/busqueda", methods=["GET", "POST"])
+def busqueda():
+    return render("busqueda.html")
+
+@app.route("/detallepost", methods=["GET", "POST"])
+def detalle():
+    return render("detallepost.html")
+
+@app.route("/perfilusuario/<id_usuario>", methods=["GET", "POST"])
+def busqueda_usuario(id_usuario):
+    return render("busqueda.html")
+    
+
+@app.route("/feed", methods=["GET"])
+def feed():
+    return render("feed.html")
+
+@app.route("/feed/<id_post>", methods=["GET", "POST"])
+def detalle_post(id_post):
+    return render("detallepost.html")
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
