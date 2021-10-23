11a1ca0e93195b34dd3bcc4b3bc1c56548f4004e
Willy Corzo <wjcorzol@gmail.com>
2021-10-23 02:51:14 -0500

    Se termino formulario Login y se adelanto formulario Regi

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, flash, jsonify
 from flask import render_template as render
-from formularios import Login
+from formularios import Login, Registro
 from markupsafe import escape
 import os
 
@@ -9,16 +9,37 @@ app= Flask(__name__)
 app.secret_key = os.urandom(24)
 
 @app.route("/", methods=["GET", "POST"])
+@app.route("/login/", methods=["GET", "POST"])
 def inicio():
     login = Login()
     if request.method == "GET":
-        return render("login_copy.html")
+        return render("login_copy.html",form=login, titulo = "Inicio de Sesión")
     else:
-        return render("login_copy.html")
+            #recuperar información del formulario
+        usuario = escape(request.form['usr'])
+        password = escape(request.form['pwd'])
+        #Validacion de datos
+        if len(usuario.strip()) == 0:
+            flash('Campo Usuario es requerido')
+
+        if len(password.strip()) == 0:
+            flash('Campo Contraseña es requerido')
+        #tomar decisiones
+        usuarios = ['willy_corzo','diomedez_diaz', 'deyvis_arrieta']
+        contrasenas = ['123456789','1234567890', '1234567891']
+        if usuario in usuarios and password == contrasenas[usuarios.index(usuario)]:
+            return render("feed.html")# debe redirecionar a la ruta no mostrar el html desde la ruta login
+        else:
+            flash('Usuario o clave invalida')
+            frm = Login()
+            return render('login_copy.html', form = login, titulo = "Inicio de Sesión")
+
 
 @app.route("/registro/", methods=["GET", "POST"])
-def registro():
-    return render("registro.html")
+def crearUsuario():
+    registro = Registro()
+
+    return render("registro_copy.html",form=registro)
 
 @app.route("/dashboard/", methods=["GET", "POST"])
 def dashboard():
--- formularios.py
+++ formularios.py
@@ -5,9 +5,10 @@ from wtforms.validators import InputRequired, EqualTo
 from wtforms.fields.html5 import EmailField, TelField, DateField
 
 class Login(FlaskForm):
-    usr = TextField('Usuario *',validators= [InputRequired(message='El campo Usuario es requerido')])
-    pwd = PasswordField('Clave *',validators= [InputRequired(message='El campo Clave es requerido')])
-    btn = SubmitField('Login')
+    usr = TextField('Usuario *', render_kw={"placeholder": "Usuario"})
+    pwd = PasswordField('Clave *', render_kw={"placeholder": "Contraseña"})
+    btnLogin = SubmitField('Login')
+    
 
 class CClave(FlaskForm):    
     act = PasswordField('Clave actual *',validators= [InputRequired(message='El campo Clave es requerido')])
@@ -16,17 +17,17 @@ class CClave(FlaskForm):
     btn = SubmitField('Cambiar')
 
 class Registro(FlaskForm):
-    usr = TextField('Usuario *',validators= [InputRequired(message='El campo Usuario es requerido')])
-    mail = EmailField('Email *', validators=[InputRequired(message='El campo Correo electrónico es requerido')])
-    pnombr = TextField('Primer Nombre *',validators= [InputRequired(message='El campo Primer Nombre es requerido')])
-    snombr = TextField('Segundo Nombre')
-    priapellido = TextField('Primer Apellido *',validators= [InputRequired(message='El campo Primer Apellido es requerido')])
-    segapellido = TextField('Segundo Apellido')
-    #sexo = RadioField('Sexo *',validators= [InputRequired(message='El campo Sexo es requerido')], choices='M',choices='F', choices ='Otro')
-    telefono = TelField('Telefono *',validators= [InputRequired(message='El campo Telefono es requerido')])
-    fechanac = DateField('Fecha de Nacimiento *',validators= [InputRequired(message='El campo Fecha de Nacimiento es requerido')])
-    cla = PasswordField('Contraseña *',validators= [InputRequired(message='El campo Contraseña es requerido')])
-    ver = PasswordField('Verifica la Contraseña *',validators= [InputRequired(message='El campo Verifica la Contraseña es requerido'), EqualTo(cla,message='La nueva clave y su verificación no coinciden')])
+    usr = TextField('Usuario', render_kw={"placeholder": "Usuario"}, validators= [InputRequired(message='El campo Usuario es requerido')])
+    mail = EmailField('Email', render_kw={"placeholder": "E-mail"}, validators=[InputRequired(message='El campo Correo electrónico es requerido')])
+    pnombr = TextField('Primer Nombre', render_kw={"placeholder": "Primer Nombre"},validators= [InputRequired(message='El campo Primer Nombre es requerido')])
+    snombr = TextField('Segundo Nombre', render_kw={"placeholder": "Segundo Nombre"})
+    priapellido = TextField('Primer Apellido', render_kw={"placeholder": "Primer Apellido"},validators= [InputRequired(message='El campo Primer Apellido es requerido')])
+    segapellido = TextField('Segundo Apellido', render_kw={"placeholder": "Segundo Apellido"})
+    sexo = RadioField('Sexo',validators= [InputRequired(message='El campo Sexo es requerido')], choices=[('M', 'M'),('F', 'F'),('O', 'Otro')])
+    telefono = TelField('Telefono *', render_kw={"placeholder": "Telefono"},validators= [InputRequired(message='El campo Telefono es requerido')])
+    fechanac = DateField('Fecha de Nacimiento',validators= [InputRequired(message='El campo Fecha de Nacimiento es requerido')], format ='%d-%m-%Y')
+    cla = PasswordField('Contraseña *', render_kw={"placeholder": "Contraseña"},validators= [InputRequired(message='El campo Contraseña es requerido')])
+    ver = PasswordField('Verifica la Contraseña *', render_kw={"placeholder": "Verifica Contraseña"},validators= [InputRequired(message='El campo Verifica la Contraseña es requerido'), EqualTo(cla,message='La nueva clave y su verificación no coinciden')])
     
-    btn = SubmitField('Registrarme')
-    btn = SubmitField('Cancelar')
+    btnRegistro = SubmitField('Registrarme')
+    
